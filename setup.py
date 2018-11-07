f90ad986c4ce099fe597dc133d297d84a9030621
Nikos Mavrakis <mavrakis.n@gmail.com>
2018-11-07 12:44:42 +0200

    drop support for Django 1.10

--- setup.py
+++ setup.py
@@ -14,10 +14,9 @@ setup(
     author='Nick Mavrakis',  # optional
     author_email='mavrakis.n@gmail.com',  # optional
     classifiers=[
-        'Development Status :: 4 - Beta',
+        "Development Status :: 5 - Production/Stable",
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
@@ -29,7 +28,7 @@ setup(
     ],  # optional
     python_requires='>=3.6',  # optional
     keywords='django progressive image field',  # optional, space separated keywords
-    install_requires=['Django>=1.10', 'Pillow>=4.0.0'],  # optional
+    install_requires=['Django>=1.11', 'Pillow>=4.0.0'],  # optional
     zip_safe=False,
     include_package_data=True,
 )
